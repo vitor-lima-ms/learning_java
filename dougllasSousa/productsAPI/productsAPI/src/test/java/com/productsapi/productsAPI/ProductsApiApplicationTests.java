e00034b2d386b35034e49588a623fbf700e310fb
Vitor Lima <viitorlima2000@gmail.com>
2025-04-17 12:08:10 -0300

    Initializing the project

---
+++ dougllasSousa/productsAPI/productsAPI/src/test/java/com/productsapi/productsAPI/ProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productsapi.productsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
