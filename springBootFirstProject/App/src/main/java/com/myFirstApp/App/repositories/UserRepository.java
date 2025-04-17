aef1bba095f306f2d1cf4341ef8be09ece43631c
Vitor Lima <viitorlima2000@gmail.com>
2025-04-17 14:25:16 -0300

    Directory delete

--- springBootFirstProject/App/src/main/java/com/myFirstApp/App/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.myFirstApp.App.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.myFirstApp.App.entities.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-	
-}
