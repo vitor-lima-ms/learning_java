7a523ded1ed9a20b7ce87f22f7c3d211d8bb1482
Vitor Lima <viitorlima2000@gmail.com>
2025-04-17 14:25:56 -0300

    Update ProductsApiApplication.java

--- dougllasSousa/productsAPI/productsAPI/src/main/java/com/productsapi/productsAPI/ProductsApiApplication.java
+++ dougllasSousa/productsAPI/productsAPI/src/main/java/com/productsapi/productsAPI/ProductsApiApplication.java
@@ -1,10 +1,12 @@
 package com.productsapi.productsAPI;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;;
 
 @SpringBootApplication
 public class ProductsApiApplication {
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductsApiApplication.class, args);
